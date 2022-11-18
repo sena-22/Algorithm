b7d02c84af964c058709666707501f92da149b3e
sena <110877564+sena-22@users.noreply.github.com>
2022-11-18 22:02:52 +0900

    [Silver II] Title: 스택 수열, Time: 284 ms, Memory: 41732 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -12,28 +12,56 @@ const input = fs
 const [n, ...nums] = input;
 const numbers = nums.map((i) => Number(i));
 
+//solution 1
+// function solution(n, numbers) {
+//   const stack = [];
+//   let answer = "";
+//   let count = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     const number = numbers.shift();
+
+//     while (count <= number) {
+//       stack.push(count++);
+//       answer += "+ ";
+//     }
+
+//     const popedItem = stack.pop();
+//     if (popedItem !== number) {
+//       return "NO";
+//     }
+
+//     answer += "- ";
+//   }
+//   return answer.split(" ").join("\n");
+// }
+
+//solution 2
 function solution(n, numbers) {
   const stack = [];
-  let answer = "";
   let count = 1;
+  let answer = "";
 
   for (let i = 0; i < n; i++) {
-    const number = numbers.shift();
-
-    while (count <= number) {
-      stack.push(count++);
-      answer += "+ ";
+    const number = numbers[i]; //현재 수열 값
+    if (number >= count) {
+      while (number >= count) {
+        stack.push(count++);
+        answer += "+ ";
+      }
+      stack.pop();
+      answer += "- ";
+    } else {
+      const popedItem = stack.pop();
+      if (popedItem > number) {
+        return "NO";
+      } else {
+        answer += "- ";
+      }
     }
-
-    const popedItem = stack.pop();
-    if (popedItem !== number) {
-      return "NO";
-    }
-
-    answer += "- ";
   }
   return answer.split(" ").join("\n");
 }
-
+//제출
 const answer = solution(n, numbers);
 console.log(answer);

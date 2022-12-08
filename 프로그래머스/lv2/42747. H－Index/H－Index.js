e2d489bb7795a8ac4c2bdeefa9f10bc897c864b0
sena <110877564+sena-22@users.noreply.github.com>
2022-12-08 20:05:32 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,20 +1,10 @@
-//9번 케이스 통과 X
+
 function solution(citations) {
     citations = citations.sort((a,b) => b-a); 
     
     for(let i = 0; i < citations.length ; i++ ) { 
         if( i +1 >= citations[i] ) return i; 
     }
-    return citations.length
+    return citations.length;
 }
 
-
-// function solution(citations) {
-//     citations = citations.sort((a, b) => b - a);
-//     let i = 0;
-//     while (i + 1 <= citations[i]) { //인용된 숫자가 i번째 숫자와 같아질 때까지 i++
-//         i++;
-//     }
-//     return i;
-
-// }

d7843f8ed8482f53822010bd44960d8df601873b
sena <110877564+sena-22@users.noreply.github.com>
2023-02-22 14:50:37 +0900

    [level 1] Title: 약수의 합, Time: 39.50 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,12 +1,9 @@
 function solution(n) {
-    let result= 0
-    let i = 0
-
-   while (i < n) {
-       if (n % i === 0)  result += i
-       else result
-        i++  
-       console.log(result)
-   }
-    return result + n
+    let sum = 0;
+   for(let i=0;i<=n; i++) 
+       {
+           if(n % i === 0) {
+               sum += i;
+           }
+       } return sum;
 }

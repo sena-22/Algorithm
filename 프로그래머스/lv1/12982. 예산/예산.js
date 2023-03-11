3433bd87c3c32bd430ee3e94d3a45f9bbbfd5fdf
sena <110877564+sena-22@users.noreply.github.com>
2023-03-11 12:06:08 +0900

    [level 1] Title: 예산, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+  let sum = 0
+  let i = 0
+  d.sort((a, b) => a - b)
+  while (sum < budget) {
+    if (sum + d[i] > budget) return i
+    if (i > d.length - 1) return i
+    sum += d[i]
+    i++
+  }
+  return i
+}

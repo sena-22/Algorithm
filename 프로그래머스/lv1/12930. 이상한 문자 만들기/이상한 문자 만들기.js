b6020901b75883f26e5b69f5124eaf467ad0e01a
sena <110877564+sena-22@users.noreply.github.com>
2023-03-15 10:12:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -2,7 +2,7 @@ function solution(s) {
       return  s
          .split(' ')
          .map(i=> 
-                i !== ' ' &&  i.split('')
+                 i.split('')
                  .map((j,idx)=> idx % 2 ===0 ? j.toUpperCase():j.toLowerCase())
                  .join('')
         )

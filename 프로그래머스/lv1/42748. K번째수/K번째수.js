75ff3a7be81a68383f3b94a1b13f569936b03c43
sena <110877564+sena-22@users.noreply.github.com>
2023-03-22 12:28:29 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,6 @@
 const solution = (array, commands) => {
-   
-     return commands.map((command)=>{     
-         
-        const [i,j,k]=command
-        let newArr = array.slice(i-1,j) 
-        return newArr.sort((a,b)=>a-b)[k-1]
-     })
+  return commands.map(([i,j,k]) => {
+    let newArr = array.slice(i - 1, j)
+    return newArr.sort((a, b) => a - b)[k - 1]
+  })
 }

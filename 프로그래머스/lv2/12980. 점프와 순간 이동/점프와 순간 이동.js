7308d6ff317ca6f5cf03d228a95425093b5c5178
sena <110877564+sena-22@users.noreply.github.com>
2023-05-27 07:30:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    
+    let count = 0
+
+    while(n > 0) {
+        
+        if(n%2 !== 0){
+            n = (n - 1) / 2
+            count++
+        } else {
+            n = n / 2
+        }
+    }
+    return count
+}

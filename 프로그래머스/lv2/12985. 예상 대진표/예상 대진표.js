0e25c8e5e9df991af7bc11c35809d16f3c2032df
sena <110877564+sena-22@users.noreply.github.com>
2023-05-30 10:42:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,18 +1,18 @@
 function solution(n,a,b) {
     let count = 1
+    
     while(a>2 ||b>2) {
         
-    if(a%2 && a+1 ===b ) return count 
+    if(a%2 && a+1 ===b ) return count // 3 4 // 3 4 / 5 6 
     if(b%2 && b+1 ===a ) return count 
-        
-        
+            
         count++
         
-        if(a%2) a++
+        if(a%2) a++ // 3+1 /2
         if(b%2) b++
         
-        a = a/2
-        b = b/2
+        a = a/2 //2
+        b = b/2 //4 
     }
     return count
 }

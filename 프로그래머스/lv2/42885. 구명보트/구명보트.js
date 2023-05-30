5f3657d063219e3608408f22aa509549c8346581
sena <110877564+sena-22@users.noreply.github.com>
2023-05-30 10:37:09 +0900

    [level 2] Title: 구명보트, Time: 10.28 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -4,11 +4,10 @@ function solution(people, limit) {
     let cnt = 0
     
     for(let i=0, j=people.length-1; i<=j; i++) {
-        if(people[i]+people[j]<=limit) {
-            j--
- 
-        }   
-      cnt++
+        if(people[i]+people[j]<=limit) { //limit보다 작거나 같으면
+            j-- //무거운 사람도 빠짐
+        }   //아니면 가벼운 사람만 빠짐
+      cnt++ 
     }
     return cnt
 }

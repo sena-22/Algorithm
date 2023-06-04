8cf71a2b52fbba7c58b1083416872fdf38a35bdc
sena <110877564+sena-22@users.noreply.github.com>
2023-06-04 16:37:54 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -3,14 +3,13 @@ function solution(brown, yellow) {
     const getBrown = (x,y) => (x+y)*2 + CORNER
 
     for(let i=0;i<=yellow;i++) {
-        let yellow_x = 0
-        let yellow_y = 0
+        let yellow_x = 0, yellow_y = 0
         
-        if(yellow % i=== 0) {
-            yellow_x = parseInt(yellow/i)
+        if(yellow % i === 0) {
+            yellow_x = yellow/i
             yellow_y = i
             
-            let  expectedBrown = getBrown( yellow_x,yellow_y)
+            let expectedBrown = getBrown(yellow_x,yellow_y)
             
             if(expectedBrown === brown) {
                 return [yellow_x+2,yellow_y+2].sort((a,b)=>b-a)

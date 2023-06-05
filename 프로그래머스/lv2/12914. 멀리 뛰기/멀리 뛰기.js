c6de8e95df24505f76076a616390dcd7c0314fca
sena <110877564+sena-22@users.noreply.github.com>
2023-06-05 10:16:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,10 @@
 function solution(n) {
     let count = 0
-    
     let fibo = new Array(n+1).fill(1)
+    
     for(let i=2;i<fibo.length;i++) {
-        fibo[i]=(fibo[i-1]+fibo[i-2]) % 1234567
+        fibo[i] = (fibo[i-1]+fibo[i-2]) % 1234567
     } 
-      return fibo[n] % 1234567
+    
+     return fibo[n] 
 }

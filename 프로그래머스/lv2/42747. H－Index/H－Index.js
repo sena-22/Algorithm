e63c08aa05e2aa8b68421127cd8e8c2bb33bd91d
sena <110877564+sena-22@users.noreply.github.com>
2023-06-05 10:46:29 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,23 +1,9 @@
 
 function solution(citations) {
-     citations.sort((a,b) => b-a); 
+    citations.sort((a,b)=>b-a)
     
-    for(let i = 0; i < citations.length ; i++ ) { 
-        if( i +1 > citations[i] ) return i; 
+    for(let i=0; i<citations.length;i++) {
+        if(i>=citations[i]) return i
     }
-    return citations.length;
+    return citations.length
 }
-
-// function solution(citations) {
-//     citations = citations.sort((a,b) => b-a); // [6,5,3,1,0]
-    
-//     for(let i= 0; i < citations.length ; i++ ) {
-        
-//         if(i+1 >= citations[i]) return i+1; //i
-        
-//         /* 1. 1 >= 6 (x) //여기서 i+1은 h번 이상 인용된 논문의 개수
-//          * 2. 2 >= 5 (x)
-//          * 3. 3 >= 3 (o) //이 경우는 3번 이상 인용된 논문이 3개 return 3
-//         */ 
-//     }
-// }

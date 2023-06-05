3f6812beabe34effb7eb08d308879dd5b9fbb169
sena <110877564+sena-22@users.noreply.github.com>
2023-06-05 10:25:08 +0900

    [unrated] Title: 귤 고르기, Time: 67.72 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -4,19 +4,21 @@ function solution(k, tangerine) {
     let count = 0
     let sum = 0
     
+    // Map(5) { 1 => 1, 3 => 2, 2 => 2, 5 => 2, 4 => 1 }
     tangerine.forEach((el)=>{
         let prev = map.get(el)
-        map.set(el, prev ? map.get(el)+1 : 1)
+        map.set(el, prev ? prev + 1 : 1)
     })
     
+
+    // Map(5) { 3 => 2, 2 => 2, 5 => 2, 1 => 1, 4 => 1 }
     const sortedMap = new Map([...map.entries()].sort((a,b)=> b[1]-a[1]))
-    const type = Array.from(sortedMap.values())
     
-    console.log(type)
+    const type = Array.from(sortedMap.values()) // [ 2, 2, 2, 1, 1 ]
     
-    for (let num of type) {
+    for (let num of type) { //2
         count++
-        sum+=num
+        sum+=num // 2 + 2 + 2
         
         if(sum >=k) break
     }

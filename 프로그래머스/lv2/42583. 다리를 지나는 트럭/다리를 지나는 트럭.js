de102e7be379e57abec006026338f54e35b425c3
sena <110877564+sena-22@users.noreply.github.com>
2023-06-15 09:21:01 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,22 @@
+function solution(bridge_length, weight, truck_weights) {
+    
+    const bridge = new Array(bridge_length).fill(0)
+    let time = 0
+    let total_weight = 0
+    let cur_truck = 0
+ 
+    while(bridge.length) {
+            total_weight -= bridge.shift()  
+            if(truck_weights.length) { 
+                if(total_weight + truck_weights[0] <= weight) {
+                    let cur_truck = truck_weights.shift()
+                    bridge.push(cur_truck)
+                    total_weight += cur_truck
+                }  else {
+                    bridge.push(0)
+                }   
+             }
+       time++  
+    }
+    return time
+}

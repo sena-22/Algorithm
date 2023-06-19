7ea90ecbeb67f1f17a95536255476b9ded9feb31
sena <110877564+sena-22@users.noreply.github.com>
2023-06-19 11:31:08 +0900

    [level 2] Title: 프로세스, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -3,19 +3,19 @@ function solution(priorities, location) {
     const location_arr = []
     
     for(let i=0; i<priorities.length;i++) {
-        location_arr.push(i)
+        location_arr.push(i) // [0,1,2,3]
     }
    
     while(priorities.length !== 0) {
-         let cur_process = priorities.shift() 
-         let max_process = Math.max(...priorities)          
+         let cur_process = priorities.shift() // 2 1 3 2
+         let max_process = Math.max(...priorities)  //3 //2      
 
         if(max_process>cur_process) { 
-            priorities.push(cur_process)
-            location_arr.push(location_arr.shift())
+            priorities.push(cur_process) // [1,3,2,2]  [3,2,2,1] // [2,2,1]
+            location_arr.push(location_arr.shift()) // [1,2,3,0] [2,3,0,1]
         } else {
-            answer++
-            if(location_arr.shift()===location) return answer
+            answer++ //1 2
+            if(location_arr.shift()===location) return answer // 2 === 2 
         }
     }
 }

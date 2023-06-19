4558b9718b855fed4c5e6cdb475394a3f0eb319f
sena <110877564+sena-22@users.noreply.github.com>
2023-06-20 08:56:44 +0900

    [level 2] Title: 소수 찾기, Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,27 @@
+function solution(numbers) {
+
+    let answer = []
+    let arr = new Array(numbers.length).fill(0)
+
+    const isPrimeNumber = (x) => {
+        if(x === 0 || x === 1) return false
+        for(let i=2; i<= Math.floor(Math.sqrt(x));i++) {
+            if(x % i ===0) return false
+        }
+        return true
+    }
+    
+    const dfs = (cur) => {
+        answer.push(+cur)
+        for(let i=0;i<numbers.length;i++) {
+            if(arr[i]===0) {
+                arr[i] = 1
+                dfs(cur+numbers[i])
+                arr[i]=0
+            }
+        }
+    }
+    dfs('')
+    answer.shift()
+    return [...new Set(answer)].filter(v=>isPrimeNumber(v)).length
+}

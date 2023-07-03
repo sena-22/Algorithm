a88131f483a9073b3794ff73011766e0a59912ec
sena <110877564+sena-22@users.noreply.github.com>
2023-07-03 11:03:08 +0900

    [level 2] Title: 모음 사전, Time: 2.94 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,22 +1,25 @@
 function solution(word) {
     let str = ''
-    const dic = []
+    const dic = [] // 'A', 'E'....'AA', 
+   
     
-    const dfs = (word, leng, dic) => { 
-       const vs = [..."AEIOU"]
+    const dfs = (word, leng, dic) => { // '', 1, [] // 'A',1,[]
+       const vs = [..."AEIOU"] 
        
-       if(leng === word.length) {
-          dic.push(word)
-           return
+       if(leng === word.length) { // 1, 0  // 1,1
+          dic.push(word) // 'A' // 'E'
+           return // return 13 line
        }
-       vs.forEach((v)=>{
-           dfs(word+v, leng, dic )
+       vs.forEach((v)=>{ //'A' // 'E'
+           dfs(word+v, leng, dic ) // ''+'A', 1, [] // 'E', 1, []
        })
    }
     
-    for(let i=1;i<=5; i++) {
-        dfs(str,i,dic)
+    for(let i=1;i<=5; i++) { // 'AA'
+        dfs(str,i,dic)  //'', 1, []
     }
+    
+    
     return dic.sort().indexOf(word)+1
 
 }

020d82b56236ed7934cd1be37ddd56c35568fe7f
sena <110877564+sena-22@users.noreply.github.com>
2023-07-03 10:38:12 +0900

    [level 2] Title: 피로도, Time: 1.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -7,13 +7,13 @@ function solution(k, dungeons) {
         if(dungeon.length === 1) return [dungeon]
       
         for (let i=0;i<dungeon.length;i++) {
-            const cur = dungeon[i]
+            const cur = dungeon[i] //[80,20]  // [30,10]
             const remain = dungeon.slice(0,i).concat(dungeon.slice(i+1))
-            const remain_permuted = permute(remain)
+            const remain_permuted = permute(remain) // [[50,40]] 
             
-            for(let j=0;j<remain_permuted.length;j++) {
-                const permutedArr = [cur].concat(remain_permuted[j])
-                result.push(permutedArr)
+            for(let j=0;j<remain_permuted.length;j++) { // [30,10], [50,40],
+                const permutedArr = [cur].concat(remain_permuted[j]) // [50,40], [30,10]
+                result.push(permutedArr) 
             }
         }
         return result 
@@ -23,14 +23,13 @@ function solution(k, dungeons) {
        dungeonPermutation.forEach((permutation) => {
            let count = 0
            let rest = k
-           permutation.forEach((el)=> {
-                let [necessary, use] = el
-                if(necessary > rest) return 
+           permutation.forEach(([necessary, use])=> {
+                if(necessary > rest || rest - use < 0  ) return 
                 rest -= use
-                if(rest<0) return
                 count++
            })
            countArr.push(count)
        })
+    
     return Math.max(...countArr);
 }

363dad4322484feefe43b6769e121cd140e15426
sena <110877564+sena-22@users.noreply.github.com>
2023-07-10 10:18:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,14 @@
-function solution(number, k) {
-    const stack = []  // ['4','3']
+function solution(number, k) { // "1924"
+    const stack = []  // 
 
-    for (num of number) { // '4', '3'
+    for (num of number) { // '1', '9', '2', '4'
         while(k > 0 && !!stack.length && stack[stack.length-1] < num) {  
                 stack.pop() 
-                k--
+                k-- // 1
         }
         stack.push(num) 
     }
 
-    return stack.slice(0,number.length-k).join('')
+    return stack.slice(0,number.length-k).join('') //  ['9','4']
     
 }

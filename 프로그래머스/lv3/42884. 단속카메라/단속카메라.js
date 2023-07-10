0a38e805a836c026942a840fd02ff614299aa32f
sena <110877564+sena-22@users.noreply.github.com>
2023-07-10 10:36:28 +0900

    [level 3] Title: 단속카메라, Time: 18.65 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
@@ -2,13 +2,17 @@ function solution(routes) {
     let count = 0 
     let camera = -30001
     
-    routes.sort((a,b)=>a[1]-b[1])
+    routes.sort((a,b)=>a[1]-b[1]) // 진출 시점 기준으로 오름차순
+    // console.log(routes) // [ [ -20, -15 ], [ -18, -13 ], [ -14, -5 ], [ -5, -3 ] ]
     
     routes.forEach(([come,out]) => {
-       if(camera < come) { 
-           camera = out
-           count++
+          //-30001 < -20(true) => -15 < -18(false) => -15 < -14(true)  => -5 < -5 (false)
+       if(camera < come) { // 카메라가 진입 시점보다 뒤에 있으면  
+           camera = out // 진출 시점으로 옮겨준다  -15 => -5
+           count++ 
        }
+        
+        // -30001 
     })
    
     return count

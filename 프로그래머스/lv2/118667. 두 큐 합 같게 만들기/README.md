924ecf37754da9e084719c08a0bdfd930d69ab9b
sena <110877564+sena-22@users.noreply.github.com>
2023-07-11 11:15:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 18.22 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 49.3 MB, 시간: 21.62 ms
+메모리: 49.2 MB, 시간: 18.22 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -2,18 +2,18 @@ function solution(queue1, queue2) {
     let answer = 0;
     let count = queue1.length + (queue2.length*1.2)
 
-    let q1T =0
-    let q2T = 0
-    
-    for(let i=0;i<queue1.length;i++) {
-        q1T += queue1[i]
+    const getTotal = (q) => {
+        let total = 0
+        for(let i=0;i<q.length;i++) {
+            total += q[i]
+        }
+        return total
     }
     
-   for(let i=0;i<queue2.length;i++) {
-        q2T += queue2[i]
-    }
+    let q1T = getTotal(queue1)
+    let q2T = getTotal(queue2)
     
-    if (q1T+q2T % 2 === 1) return -1
+    // if (q1T+q2T % 2 === 1) return -1
     
     let idx1 = 0
     let idx2 = 0

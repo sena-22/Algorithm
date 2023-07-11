61fe19754b6d69d8ded8e1e6aa935ce9610cbcb4
sena <110877564+sena-22@users.noreply.github.com>
2023-07-11 12:58:58 +0900

    [level 3] Title: 여행경로, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -2,14 +2,14 @@ function solution(tickets) {
     let route = []
     const visited = []
     const stack = []
-
+    
    tickets.sort()
 
     const dfs = (airport, count) => {
         stack.push(airport)
         
         if(count === tickets.length) {
-            route = stack
+            // route = stack
             return true
         }
            
@@ -19,14 +19,11 @@ function solution(tickets) {
                 if(dfs(tickets[i][1], count+1)) return true
                 visited[i] = false 
         }        
-    }
-    
+    } 
     stack.pop()
     return false
- 
 }
- 
     dfs("ICN",0)
-    return route
+    return stack
 }  
     

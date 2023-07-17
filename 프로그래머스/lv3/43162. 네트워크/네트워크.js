0e83bb8afc388525f27bb0e85f17425b9eac0caa
sena <110877564+sena-22@users.noreply.github.com>
2023-07-17 10:10:53 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,21 +1,22 @@
 function solution(n, computers) {
     let network = 0
-    const visited =[]
+    const visited = []
     
     const dfs = (i) => { 
         visited[i] = true
         const linked = computers[i] // [1, 1, 0]
-         linked.forEach((node,idx)=> {
-             if(!visited[idx] && node === 1) {
+
+         linked.forEach((node,idx)=> { 
+             if(!visited[idx] && node === 1) { // 방문하지 않고, 연결되어 있으면 i번째 탐색
                  return dfs(idx)
              }
           })
     }
     
      computers.forEach((computer,i)=> {
-         if(!visited[i]) {
+         if(!visited[i]) { 
               dfs(i) 
-             network++
+             network++ // dfs를 돌 동안 방문하지 않았으면 network 추가
          }
     })
 

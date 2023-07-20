536cb2942bed0a2ca037f079694bb1d40f40f8c2
sena <110877564+sena-22@users.noreply.github.com>
2023-07-20 09:28:41 +0900

    [level 3] Title: 입국심사, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -2,12 +2,12 @@ function solution(n, times) {
     //times 오름차순 정렬
     times.sort((a,b)=>a-b)
     
-    let left = 1 // 최소 시간
-    let right = n*times[times.length-1] // 최대 시간
-    let answer = right
+    let min = 1 // 최소 시간
+    let max = n * times[times.length-1] // 최대 시간
+    let answer = max
     
-    while(left <= right) {
-       let mid = Math.floor((left+right)/2)
+    while(min <= max) {
+       let mid = Math.floor((min+max)/2)
        let cnt = 0
        times.forEach((time)=>{
            cnt += Math.floor(mid/time) // 한 심사대에 몇 명인지
@@ -18,9 +18,9 @@ function solution(n, times) {
        })
         
         if(cnt >= n) {
-            right = mid -1
+            max = mid -1
         } else {
-            left = mid +1
+            min = mid +1
         }
     }    
     return answer;

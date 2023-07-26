3f7c1ba11256b3505870bff459d0072299004d39
sena <110877564+sena-22@users.noreply.github.com>
2023-07-26 12:12:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -1,28 +1,19 @@
-const fs = require("fs");
-const readFileSyncAddress = "/dev/stdin";
-const input = fs
-  .readFileSync(readFileSyncAddress)
-  .toString()
-  .trim()
-  .split("\n");
-
-const n = input.map((el) => parseInt(el));
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const n = input.map((el) => parseInt(el))
 
 const solution = (n) => {
-  let fake = n.reduce((acc, cur) => acc + cur) - 100;
-  let answer = [];
+  const fake = n.reduce((acc, cur) => acc + cur, 0) - 100
+  let answer = []
 
   for (let i = 0; i < n.length - 1; i++) {
-    for (let j = i + 1; j < n.length; j++) {
+    for (let j = 0; j < n.length; j++) {
       if (n[i] + n[j] === fake) {
-        answer = n.filter((n, idx) => idx !== i && idx !== j);
-        break;
+        answer = n.filter((_, idx) => idx !== i && idx !== j)
       }
     }
   }
-  return answer.sort((a, b) => a - b).join("\n");
-};
+  return answer.sort((a, b) => a - b).join('\n')
+}
 
-//제출
-const answer = solution(n);
-console.log(answer);
+const answer = solution(n)
+console.log(answer)

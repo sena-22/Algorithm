3ff20fd036acb98b5a9ca4c4ec31105126e25fd4
sena <110877564+sena-22@users.noreply.github.com>
2023-07-31 10:43:02 +0900

    [level 3] Title: 순위, Time: 55.45 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.js
+++ 프로그래머스/lv3/49191. 순위/순위.js
@@ -8,27 +8,34 @@ function solution(n, results) {
         graph[win][win] = 0
         graph[lose][lose] = 0
     })
+    
     const range = [...Array(n).keys()].map(e=>e+1) // [1,2,3,4,5]
     
     // 플로이드 와샬
-    for(const mid of range) {
-        for(const a of range) {
-            for(const b of range) {
-                if(graph[a][mid] === 1 && graph[mid][b] === 1) graph[a][b] = 1
-                if(graph[a][mid] === -1 && graph[mid][b] === -1) graph[a][b] = -1
+    // 1을 거쳐가는 경우
+    // 2->3
+    // 2->1->3
+    for(const k of range) { // k = 거쳐가는 노드
+        for(const a of range) { // i = 출발 노드
+            for(const b of range) { // j = 도착 노드 
+                // console.log(k,a,b, graph[a][k], graph[k][b])
+                if(graph[a][k] === 1 && graph[k][b] === 1) graph[a][b] = 1
+                if(graph[a][k] === -1 && graph[k][b] === -1) graph[a][b] = -1
             }
         }
     }
     
-    for (const i of range) {
-        let hasF = false
-        for(const j of range) {
+    console.log(graph)
+    
+    for (const i of range) { //[1,2,3,4,5]
+        let isF = false
+        for(const j of range) { //[1,2,3,4,5]
             if(graph[i][j] === false) {
-                hasF = true
+                isF = true
                 break
             }
         }
-        count += hasF ? 0 : 1;
+        count += isF ? 0 : 1;
     }
     
     return count

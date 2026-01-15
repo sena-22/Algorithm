59d8cde98639ec371d9d7510a82f54cfaeb5df7f
sena <110877564+sena-22@users.noreply.github.com>
2026-01-16 06:52:04 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 15.63 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.py
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.py
@@ -1,21 +1,40 @@
 def solution(beginning, target):
     n = len(beginning)
     m = len(beginning[0])
-    INF = 10**9
+    INF = float('inf')
     answer = INF
-
-    for mask in range(1 << n):           
+    # 행이 n개일때 선택/비선택의 경우는 2**n
+    # 행이 3일때 8가지 경우 
+    # 0이 안뒤집음, 1이 뒤집음
+    # 000 / 001 / 010 / 011 / 100 / 101/ 110 / 111
+    for mask in range(2**n):    
+        # 값만 복사
         board = [row[:] for row in beginning]
         cnt = 0
-        print(mask)
+
         for i in range(n):
-            if mask & (1 << i):
-                cnt += 1
-                for j in range(m):
+            # mask = 5일때 (101₂)
+            # i = 0
+            # &(and)는 둘다 1일때만 1
+            # 1<<0(1을 왼쪽으로 0번 이동 )
+            # mask = 00101
+            # 1<<i = 00001(i번째 비트만 1인 값)
+            # &연산 = 00001 => True
+            if mask & (1 << i): 
+                cnt += 1 # 뒤집기 횟수 추가
+                for j in range(m): # 
+                    # ^= 1 : 0 → 1, 1 → 0 (무조건 뒤집기)
+                    # i행 모든 값 반대로 바꾸기
                     board[i][j] ^= 1
 
-        for j in range(m):
-            if board[0][j] != target[0][j]:
+        # [[1, 1, 1], 행이 다르면 열 뒤집기
+        #  [0, 0, 0], 
+        #  [0, 0, 0]]
+        # [[1, 0, 1], 
+        #  [0, 1, 0], 
+        #  [0, 1, 0]]
+        for j in range(m): # 0번 열부터 m-1열까지
+            if board[0][j] != target[0][j]: # 0번 행이 다르면
                 cnt += 1
                 for i in range(n):
                     board[i][j] ^= 1

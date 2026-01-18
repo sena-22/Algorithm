ff518571134f3a6cf4fd9d6d4d15a8201535d099
sena <110877564+sena-22@users.noreply.github.com>
2026-01-18 10:26:16 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 515.85 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.py
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.py
@@ -2,10 +2,10 @@ def solution(matrix_sizes):
     answer = 0
     #행렬의 곱셈: ab * bc => ac행렬 만들어짐
     
-    # 5,3 * 3,10 * 10,6
+    # p: 5,3 * 3,10 * 10,6
     # => 겹치는 크기를 제외한 배열 => [5,3,10,6]
     
-    # 차원배열p : 첫 행의 열은 겹치지 않으므로 넣어둠
+    # 첫 행의 행은 겹치지 않으므로 넣어둠
     p = [matrix_sizes[0][0]]
     
     # 모든 행렬의 열(col)만 추가 
@@ -16,25 +16,37 @@ def solution(matrix_sizes):
     n = len(p)-1
     INF = float('inf')
     
-    # dp[i][j] = Ai부터 Aj까지 곱하는 최소 비용
-    # dp[i][j] = 왼쪽 비용 + 오른쪽 비용 + 마지막 곱 비용
     # 인덱스 1부터 사용
-    dp = [[0]*(n+1) for _ in range(n+1)]
+    dp = [[INF] * (n + 1) for _ in range(n + 1)]
     
+    # 자기자신은 0
+    for i in range(1,n+1):
+        dp[i][i] = 0
+   
     for length in range(2,n+1):
-        for i in range(1, n-length+2):
-            j = i + length -1
-            dp[i][j] = INF
-            
-            for k in range(i,j):
+        for i in range(1, n-length+2): # i:구간 시작, j:구간 끝
+            j = i + length -1 
+
+            # A1*A2*A3
+            # 1. k가 1일때 => (A1) * (A2*A3)
+            # 2. k가 2일때 => (A1*A2) * (A3)
+
+            for k in range(i,j): # 마지막 곱셈 위치
+                
+                # dp[i][j] = Ai부터 Aj까지 곱하는 최소 비용
+                
+                # dp[i][k] : k의 왼쪽 계산 비용
+                # dp[k+1][j] : k의 오른쪽 계산 비용
+                # p[i-1]*p[k]*p[j] : 마지막 두 개를 곱하는 계산 비용
+                
                 cost = dp[i][k]+dp[k+1][j]+p[i-1]*p[k]*p[j]
                 dp[i][j] = min(dp[i][j],cost)
-    
-    
-    return dp[1][n]
-    
-    
-    
 
+    # print(dp)
+    #     0  1  2  3 
+    # 0 [[0, 0, 0, 0], 
+    # 1 [0, 0, 150, 270], 
+    # 2 [0, 0, 0, 180], 
+    # 3 [0, 0, 0, 0]]
     
-    return answer
+    return dp[1][n] # A1-An까지의 최소 연산 횟수

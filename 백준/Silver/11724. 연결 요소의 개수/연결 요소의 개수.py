35a3030125499f9c09f0f44b66ff47aab6cfcef9
sena <110877564+sena-22@users.noreply.github.com>
2026-02-20 21:04:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 656 ms, Memory: 65964 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+connected = 0
+
+for _ in range(M):
+  a,b = map(int,input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+def dfs(node):
+  stack = [node]
+  visited[node] = True
+
+  while stack:
+    node = stack.pop()
+    for next_node in graph[node]:
+      if not visited[next_node]:
+        visited[next_node] = True
+        stack.append(next_node)
+
+for i in range(1,N+1):
+  if not visited[i]:
+    dfs(i)
+    connected += 1
+
+print(connected)
+

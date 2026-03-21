73ecf5ccd67109cb7490d66d76c467f27a9c6e22
sena <110877564+sena-22@users.noreply.github.com>
2026-03-21 10:51:29 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -2,14 +2,4 @@ import sys
 input = sys.stdin.readline
 
 [a,b,c]  = list(map(int,input().split()))
-
-result = 1
-
-while b > 0:
-  if b % 2 == 1:#b가 홀수면 
-    result = (result * a) % c
-
-  a = (a * a) % c # a를 제곱
-  b //= 2 # b를 반으로 나눔
-
-print(result)
+print(pow(a, b, c))

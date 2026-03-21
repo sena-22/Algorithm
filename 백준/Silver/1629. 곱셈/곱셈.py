51aa77d5e806e31436678f69da8fd6cc0f45d2ea
sena <110877564+sena-22@users.noreply.github.com>
2026-03-21 11:11:47 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -2,4 +2,19 @@ import sys
 input = sys.stdin.readline
 
 [a,b,c]  = list(map(int,input().split()))
-print(pow(a, b, c))
+
+def power(a,b,c):
+  if b == 0:
+    return 1 
+
+  if b == 1:
+    return a % c
+  
+  x = power(a, b//2, c)
+
+  if b % 2 == 0:
+    return (x * x) % c
+  else:
+    return (x * x * a) % c
+  
+print(power(a,b,c))

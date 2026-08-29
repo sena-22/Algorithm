785c12a9285be2744b832b153b44e7dbfe48a591
sena <110877564+sena-22@users.noreply.github.com>
2026-08-29 10:02:19 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.53%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # 양의 정수 n이 주어졌을 때, 그 수를 이진수로 나타냈을 때 1(set bit)이 몇 개인지 반환하는 함수를 작성하시오. 이 값을 해밍 가중치(Hamming weight)라고도 합니다.
+        # 양의 정수 n이 주어졌을 때, 그 수를 이진수로 나타냈을 때 1(set bit)이 몇 개인지 반환하는 함수를 작성하시오. 
+        # 이 값을 해밍 가중치(Hamming weight)라고도 합니다.
 
-        return   bin(n)[2:].count('1') # 'ob' 제외
+        return   bin(n)[2:].count('1') # 'ob' 제외. 
